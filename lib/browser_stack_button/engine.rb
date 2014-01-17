0ad461923f7165088bfa19065fc684728ad5d9a1
Aaron Jensen <aaronjensen@gmail.com>
2014-01-16 23:22:04 -0800

    precompile assets

--- lib/browser_stack_button/engine.rb
+++ lib/browser_stack_button/engine.rb
@@ -2,5 +2,12 @@ require 'rails'
 
 module BrowserStackButton #:nodoc:
   class Engine < ::Rails::Engine #:nodoc:
+    initializer "browser_stack_button.assets.precompile" do |app|
+      app.config.assets.precompile += %w[
+        browser_stack_button.png
+        browser_stack_button.js
+        browser_stack_button.css
+      ]
+    end
   end
 end
